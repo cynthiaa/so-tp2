ec50d45708e7e29e223f224961287873f7cb29a7
lulirez <lulirez@gmail.com>
2012-11-08 17:03:11 -0300

    Definiciones de las funciones de func.c

--- src/lib/func.c
+++ src/lib/func.c
@@ -4,14 +4,50 @@
 static char* error;
 
 int cvs_checkout(char* remote)
+int check_uri(char *path)
 {
 	char* base_uri = "cvs://";
 	for (int i = 0; i < strlen(base_uri) ; ++i) {
 			if (remote[i] != base_uri[i]) {
+			if (path[i] != base_uri[i]) {
 				error = "unknown remote directory";
 				return -1;
 			}
 	}
+}
 
+
+int cvs_checkout(char* remote)
+	check_uri(remote);
+
+}
+
+
+int cvs_add(char* path)
+{
+
+
+
+}
+   
+
+int cvs_remove(char* path)
+{
+
+}
+
+
+int cvs_move(char* old, char* new)
+{
+
+}
+
+int cvs_update()
+{
+
+}
+
+int cvs_diff_all(int revision)
+{
 
 }
