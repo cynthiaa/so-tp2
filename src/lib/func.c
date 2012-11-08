a1a037175d1134fbdce773bb076db5681019bacb
lulirez <lulirez@gmail.com>
2012-11-08 17:04:36 -0300

    func.c detalles

--- src/lib/func.c
+++ src/lib/func.c
@@ -3,12 +3,10 @@
 
 static char* error;
 
-int cvs_checkout(char* remote)
 int check_uri(char *path)
 {
 	char* base_uri = "cvs://";
 	for (int i = 0; i < strlen(base_uri) ; ++i) {
-			if (remote[i] != base_uri[i]) {
 			if (path[i] != base_uri[i]) {
 				error = "unknown remote directory";
 				return -1;
@@ -17,10 +15,13 @@ int check_uri(char *path)
 }
 
 
+
 int cvs_checkout(char* remote)
+{
 	check_uri(remote);
 
 }
+
 
 
 int cvs_add(char* path)
